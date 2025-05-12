4fb3bc68134f74b0e572cd9887bb665dafb2d603
bharathautomation <soft.ittech20@gmail.com>
2025-05-12 23:05:42 +0530

    Initial Commit

---
+++ src/test/java/tests/ReadExcel.java
@@ -0,0 +1,21 @@
+package tests;
+
+import java.util.List;
+import java.util.Map;
+
+import utilities.ExcelUtil;
+
+public class ReadExcel {
+
+	public static void main(String[] args) {
+		List<Map<String,String>> data = ExcelUtil.readData("TestData.xlsx", "Sheet1");
+		
+		System.out.println(data);
+		
+		for(Map<String,String> row : data) {
+			System.out.println(row);
+		}
+
+	}
+
+}
