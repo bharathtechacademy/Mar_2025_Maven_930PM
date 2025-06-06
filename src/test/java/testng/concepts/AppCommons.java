d47ed9aa3613951a4916a45c62e95c056425991a
bharathautomation <soft.ittech20@gmail.com>
2025-06-06 22:41:32 +0530

    TestNG framework

--- src/test/java/testng/concepts/AppCommons.java
+++ src/test/java/testng/concepts/AppCommons.java
@@ -5,18 +5,18 @@ import org.testng.annotations.Test;
 
 public class AppCommons extends WebCommons{
 	
-	@Test
+	@Test(priority = 2, groups= {"Regression"},retryAnalyzer=RetryTest.class)
 	public void TestCase1() {
 		Assert.fail("Test Case 1 - Home Page Validations Failed..."); // Simulating a failure for demonstration
 		System.out.println("Test Case 1 - Home Page Validations Executed Successfully...");
 	}
 	
-	@Test
+	@Test(priority = 3, groups= {"Smoke"}, enabled=true)
 	public void TestCase2() {
 		System.out.println("Test Case 2 - Add Product to Cart Feature Validations Executed Successfully...");
 	}	
 
-	@Test(dataProvider = "loginData")
+	@Test(dataProvider = "loginData",priority = 1,dependsOnMethods = {"TestCase1"},groups= {"Regression","Smoke","Sanity"}) //
 	public void TestCase3(String username, String password) {
 		System.out.println("Test Case 3 - Login Validations Executed Successfully with..."+username+" ,"+password);
 	}
--- src/test/java/testng/concepts/WebCommons.java
+++ src/test/java/testng/concepts/WebCommons.java
@@ -9,24 +9,24 @@ import org.testng.annotations.Parameters;
 
 public class WebCommons {
 	
-	@BeforeSuite
+	@BeforeSuite(alwaysRun = true)
 	public void startReporting() {
 		System.out.println("Reporting process started.....");
 	}
 	
-	@AfterSuite
+	@AfterSuite(alwaysRun = true)
 	public void stopReporting() {
 		System.out.println("Reporting process stopped and Report generated.....");
 	}
 	
-	@BeforeMethod
+	@BeforeMethod(alwaysRun = true)
 	@Parameters({"BROWSER", "URL"})
 	public void launchBrowserAndApplication(String browser, String url) {
 		System.out.println(browser+" is launched...");
 		System.out.println(url+" is launched...");
 	}
 	
-	@AfterMethod
+	@AfterMethod(alwaysRun = true)
 	public void closeBrowser() {
 		System.out.println("Browser is closed....");
 	}
